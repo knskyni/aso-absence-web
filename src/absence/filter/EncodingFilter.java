f6a924ee1fd86b2435e660e64e46d160c4d3780a
yanakence <yanakence@yanah.jp>
2019-10-01 11:27:39 +0900

    fix: フィルター適用できていなかったのを修正

--- src/absence/filter/EncodingFilter.java
+++ src/absence/filter/EncodingFilter.java
@@ -8,7 +8,9 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
 
+@WebFilter("/*")
 public class EncodingFilter implements Filter {
     private final static String encoding = "UTF-8";
 
