df7105f9914d0d0d065d1f4613d0bd6909f02c66
Kensuke Yanai <knskyni@yanai-k.com>
2019-10-02 11:45:30 +0900

    fix: Daoを使用するモデルクラスで例外処理できていなかったのを修正

--- src/absence/model/LoginModel.java
+++ src/absence/model/LoginModel.java
@@ -6,9 +6,16 @@ import absence.model.dao.UserDao;
 public class LoginModel {
     public LoginInfoBeans login(String userId, String password) {
         UserDao userDao = new UserDao();
+        LoginInfoBeans loginInfoBeans = null;
 
-        userDao.connect();
-        LoginInfoBeans loginInfoBeans = userDao.getBy(userId, password);
+        try {
+            userDao.connect();
+            loginInfoBeans = userDao.getBy(userId, password);
+        } catch(Exception e) {
+            e.printStackTrace();
+        } finally {
+            userDao.close();
+        }
 
         return loginInfoBeans;
     }
