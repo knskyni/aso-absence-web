84d29936f921e2f311fa999f63d4a2ce449ad775
Kensuke Yanai <knskyni@yanai-k.com>
2019-10-03 10:38:41 +0900

    feat: 公欠登録確認ページを実装した

---
+++ src/absence/controller/ConfirmAbsenceRegistServlet.java
@@ -0,0 +1,48 @@
+package absence.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import absence.beans.AbsenceBeans;
+import absence.beans.LoginInfoBeans;
+
+@WebServlet("/confirmabsenceregist")
+public class ConfirmAbsenceRegistServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        /* formからの値取得 */
+        String absenceDate = request.getParameter("absence_date");
+        String companyName = request.getParameter("company_name");
+        String reason = request.getParameter("reason");
+
+        if(absenceDate == null || companyName == null || reason == null) {
+            response.sendRedirect("inputabsenceregist");
+            return;
+        }
+
+        /* セッション取得 */
+        HttpSession session = request.getSession(false);
+        LoginInfoBeans loginInfoBeans = (LoginInfoBeans)session.getAttribute("loginInfoBeans");
+
+        /* 値をBeansに格納 */
+        AbsenceBeans registAbsenceBeans = new AbsenceBeans();
+        registAbsenceBeans.setUserId(loginInfoBeans.getUserId());
+        registAbsenceBeans.setAbsenceDate(absenceDate);
+        registAbsenceBeans.setCompanyName(companyName);
+        registAbsenceBeans.setReason(reason);
+
+        /* 値の受け渡し */
+        session.setAttribute("registAbsenceBeans", registAbsenceBeans);
+
+        /* JSP読み込み */
+        RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/jsp/confirmabsenceregist.jsp");
+        dispatcher.forward(request, response);
+    }
+}
